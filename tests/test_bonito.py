09e14b48ad225416c63bddbb4f260b1c70fd8132
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2022-05-24 10:51:10 -0300

    fix: tests

--- tests/test_bonito.py
+++ tests/test_bonito.py
@@ -1,17 +1,2 @@
-import pytest
-
-from .base_client import BaseClient
-
-
-class TodoClient(BaseClient):
-    def __init__(self, client):
-        super().__init__(client, endpoint_path="todo")
-
-
-@pytest.fixture
-def todo_client(client):
-    return TodoClient(client)
-
-
-def test_delete_todo(session):
+def test_legal():
     assert 1 == 1
