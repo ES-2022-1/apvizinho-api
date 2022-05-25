798a44ab62d63864b6c664097e890ed803f4cd3f
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2022-05-25 15:05:29 -0300

    feat: improving docker support experience

--- tests/test_bonito.py
+++ tests/test_bonito.py
@@ -1,2 +1,29 @@
-def test_legal():
-    assert 1 == 1
+import json
+from datetime import datetime
+
+import pytest
+
+from .base_client import BaseClient
+
+
+class TodoClient(BaseClient):
+    def __init__(self, client):
+        super().__init__(client, endpoint_path="todo_list")
+
+
+@pytest.fixture
+def todo_client(client):
+    return TodoClient(client)
+
+
+@pytest.fixture
+def todo(make_todo):
+    return make_todo()
+
+
+def test_create_todo(todo_client):
+    data = {"nome": "string", "prazo": datetime.now()}
+    response = todo_client.create(json.dumps(data))
+
+    assert response.status_code == 200
+    assert response.json()["nome"] == "string"
