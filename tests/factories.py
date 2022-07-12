724b33bac7cf4b1aa6ac38129a18b28e0a312211
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2022-07-11 22:45:30 -0300

    fix: make review defaults

--- tests/factories.py
+++ tests/factories.py
@@ -95,19 +95,7 @@ def make_announcement(make_user, make_address):
 
 @pytest.fixture
 def make_review(make_user):
-    defaults = dict(
-        title="string",
-        description="string",
-        is_close_to_university=True,
-        is_close_to_supermarket=True,
-        has_furniture=True,
-        has_internet=True,
-        allow_pet=True,
-        allow_events=True,
-        has_piped_gas=True,
-        status=StatusEnum.ACTIVE,
-        type=AnnouncementTypeEnum.HOUSE,
-    )
+    defaults = dict(comment="string", score=4)
 
     def _make_review(user: models.Users = make_user(), **overrides):
         return models.Review(id_review=uuid.uuid4(), user=user, **{**defaults, **overrides})
