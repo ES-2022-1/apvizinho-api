dadd90241052ba8e78f736afdabcbf8a9369cf9b
MariaEduardaDeAzevedo <maria.silva@ccc.ufcg.edu.br>
2022-07-17 17:09:23 -0300

    Add test to the new score condition

--- tests/test_announcement_endpoints.py
+++ tests/test_announcement_endpoints.py
@@ -3,7 +3,11 @@ from typing import List
 
 import pytest
 
-from app.announcement.schemas.announcement import AnnouncementTypeEnum, StatusEnum, AnnouncementTagsEnum
+from app.announcement.schemas.announcement import (
+    AnnouncementTypeEnum,
+    StatusEnum,
+    AnnouncementTagsEnum,
+)
 
 from .base_client import BaseClient
 
@@ -13,7 +17,9 @@ class AnnouncementClient(BaseClient):
         super().__init__(client, endpoint_path="announcement")
 
     def list_announcements_by_filter(self, announcement_filter):
-        return self.client.post(f"{self.path}/filter", headers=self.headers, json=announcement_filter)
+        return self.client.post(
+            f"{self.path}/filter", headers=self.headers, json=announcement_filter
+        )
 
 
 @pytest.fixture
@@ -126,7 +132,8 @@ def test_list_announcements(announcement, session, client):
     assert response.status_code == 200
     assert len(response.json()) == 1
 
-def test_list_announcements_by_filter(make_user, session, client):
+
+def test_list_announcements_by_filter_1(make_user, session, client):
     user = make_user()
     session.add(user)
     session.commit()
@@ -149,7 +156,7 @@ def test_list_announcements_by_filter(make_user, session, client):
             "city": "Campina Grande",
             "number": "200",
             "complement": "",
-            "zip_code": "58429110"
+            "zip_code": "58429110",
         },
         "vacancies": [
             {
@@ -164,7 +171,7 @@ def test_list_announcements_by_filter(make_user, session, client):
                 "gender": "FEMALE",
                 "price": 0,
             }
-        ]
+        ],
     }
 
     data_anuncio2 = {
@@ -185,7 +192,7 @@ def test_list_announcements_by_filter(make_user, session, client):
             "city": "Campina Grande",
             "number": "380",
             "complement": "",
-            "zip_code": "58416543"
+            "zip_code": "58416543",
         },
         "vacancies": [
             {
@@ -200,7 +207,7 @@ def test_list_announcements_by_filter(make_user, session, client):
                 "gender": "FEMALE",
                 "price": 0,
             }
-        ]
+        ],
     }
 
     data_anuncio3 = {
@@ -236,7 +243,7 @@ def test_list_announcements_by_filter(make_user, session, client):
                 "gender": "FEMALE",
                 "price": 0,
             }
-        ]
+        ],
     }
 
     client.create(json.dumps(data_anuncio1))
@@ -244,12 +251,155 @@ def test_list_announcements_by_filter(make_user, session, client):
     client.create(json.dumps(data_anuncio3))
 
     filters = {
-        "filters": [AnnouncementTagsEnum.IS_CLOSE_TO_UNIVERSITY, AnnouncementTagsEnum.IS_CLOSE_TO_SUPERMARKET, AnnouncementTagsEnum.ALLOW_PETS, AnnouncementTagsEnum.FEMALE_GENDER]
+        "filters": [
+            AnnouncementTagsEnum.IS_CLOSE_TO_UNIVERSITY,
+            AnnouncementTagsEnum.IS_CLOSE_TO_SUPERMARKET,
+            AnnouncementTagsEnum.ALLOW_PETS,
+            AnnouncementTagsEnum.FEMALE_GENDER,
+        ]
     }
 
     response = client.list_announcements_by_filter(filters)
 
     assert response.status_code == 200
-    assert list(map(lambda x: x["title"], response.json())) == ["anuncio 3", "anuncio 1", "anuncio 2"]
-    
+    assert list(map(lambda x: x["title"], response.json())) == [
+        "anuncio 3",
+        "anuncio 1",
+        "anuncio 2",
+    ]
 
+
+def test_list_announcements_by_filter_2(make_user, session, client):
+    user = make_user()
+    session.add(user)
+    session.commit()
+
+    data_anuncio1 = {
+        "title": "anuncio 1",
+        "description": "string",
+        "is_close_to_university": True,
+        "is_close_to_supermarket": True,
+        "has_furniture": False,
+        "has_internet": False,
+        "allow_pet": True,
+        "allow_events": True,
+        "has_piped_gas": False,
+        "status": "ACTIVE",
+        "type": "HOUSE",
+        "id_user": user.id_user,
+        "address": {
+            "street": "Av. Ricardo Wagner da Silva Paz",
+            "city": "Campina Grande",
+            "number": "200",
+            "complement": "",
+            "zip_code": "58429110",
+        },
+        "vacancies": [
+            {
+                "has_bathroom": False,
+                "has_garage": True,
+                "has_furniture": False,
+                "has_cable_internet": False,
+                "is_shared_room": True,
+                "allowed_smoker": False,
+                "required_organized_person": False,
+                "required_extroverted_person": False,
+                "gender": "MALE",
+                "price": 0,
+            }
+        ],
+    }
+
+    data_anuncio2 = {
+        "title": "anuncio 2",
+        "description": "string",
+        "is_close_to_university": False,
+        "is_close_to_supermarket": True,
+        "has_furniture": True,
+        "has_internet": False,
+        "allow_pet": True,
+        "allow_events": True,
+        "has_piped_gas": False,
+        "status": "ACTIVE",
+        "type": "HOUSE",
+        "id_user": user.id_user,
+        "address": {
+            "street": "Rua Bolivia",
+            "city": "Campina Grande",
+            "number": "380",
+            "complement": "",
+            "zip_code": "58416543",
+        },
+        "vacancies": [
+            {
+                "has_bathroom": False,
+                "has_garage": False,
+                "has_furniture": True,
+                "has_cable_internet": False,
+                "is_shared_room": False,
+                "allowed_smoker": True,
+                "required_organized_person": False,
+                "required_extroverted_person": False,
+                "gender": "FEMALE",
+                "price": 0,
+            }
+        ],
+    }
+
+    data_anuncio3 = {
+        "title": "anuncio 3",
+        "description": "string",
+        "is_close_to_university": True,
+        "is_close_to_supermarket": True,
+        "has_furniture": False,
+        "has_internet": True,
+        "allow_pet": True,
+        "allow_events": True,
+        "has_piped_gas": True,
+        "status": "ACTIVE",
+        "type": "HOUSE",
+        "id_user": user.id_user,
+        "address": {
+            "street": "R. Antônio Joaquim Pequeno",
+            "city": "Campina Grande",
+            "number": "595",
+            "complement": "",
+            "zip_code": "58429105",
+        },
+        "vacancies": [
+            {
+                "has_bathroom": True,
+                "has_garage": True,
+                "has_furniture": True,
+                "has_cable_internet": True,
+                "is_shared_room": False,
+                "allowed_smoker": False,
+                "required_organized_person": True,
+                "required_extroverted_person": False,
+                "gender": "FEMALE",
+                "price": 0,
+            }
+        ],
+    }
+
+    client.create(json.dumps(data_anuncio1))
+    client.create(json.dumps(data_anuncio2))
+    client.create(json.dumps(data_anuncio3))
+
+    filters = {
+        "filters": [
+            AnnouncementTagsEnum.IS_CLOSE_TO_UNIVERSITY,
+            AnnouncementTagsEnum.IS_CLOSE_TO_SUPERMARKET,
+            AnnouncementTagsEnum.ALLOW_PETS,
+            AnnouncementTagsEnum.FEMALE_GENDER,
+        ]
+    }
+
+    response = client.list_announcements_by_filter(filters)
+
+    assert response.status_code == 200
+    assert list(map(lambda x: x["title"], response.json())) == [
+        "anuncio 3",
+        "anuncio 2",
+        "anuncio 1",
+    ]
