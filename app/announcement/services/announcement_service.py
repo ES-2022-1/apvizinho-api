7a0a6dbbf72ec788d9996ffedd2295b6b5a6ea3d
José Igor de Farias Gomes <jose.gomes@ccc.ufcg.edu.br>
2022-07-17 12:30:29 -0300

    modifica recebimento de corpo na requisicao

--- app/announcement/services/announcement_service.py
+++ app/announcement/services/announcement_service.py
@@ -8,11 +8,12 @@ from app.announcement.schemas import (
     AnnouncementView,
 )
 from app.announcement.schemas import announcement
-from app.announcement.schemas.announcement import AnnouncementCreate, AnnouncementFilter
+from app.announcement.schemas.announcement import AnnouncementCreate, AnnouncementFilter, AnnouncementTagsEnum
 from app.announcement.schemas.vacancy import GenderEnum, VacancyCreate
 from app.announcement.services.address_service import AddressService
 from app.announcement.services.vacancy_service import VacancyService
 from app.common.services.base import BaseService
+from app.common.utils.constants import UFCG_COORDINATES, RADIUS
 
 
 class AnnouncementService(
@@ -44,10 +45,9 @@ class AnnouncementService(
 
         return announcement
 
-    def filter(self, announcement_filter:AnnouncementFilter):
+    def filter(self, filters: list):
         all_announcements = self.get_all()
         ranked_announcements = dict()
-        filters = announcement_filter.filters
         filters = list(reversed(filters))
 
         for announcement in all_announcements:
@@ -67,51 +67,49 @@ class AnnouncementService(
         m = len(matched)
         n = len(filters)
 
-        UFCG_COORDINATES = (-7.216945, -35.909722) #TODO - Colocar esse dado em um arquivo de constantes
-        R = 1
         announcement_coordinates = (announcement.address.latitude,announcement.address.longitude)
         d = haversine(UFCG_COORDINATES, announcement_coordinates)
 
-        return ((m/n) * match_score) + (R - (d/R))
+        return ((m/n) * match_score) + (RADIUS - (d/RADIUS))
 
 
     def __extract_true_items(self, announcement:AnnouncementView):
 
-        #TODO - Criar um ENUM para esses itens
-
         true_items = list()
 
         if announcement.allow_events:
-            true_items.append("ALLOW_EVENTS")
+            true_items.append(AnnouncementTagsEnum.ALLOW_EVENTS)
         if announcement.allow_pet:
-            true_items.append("ALLOW_PETS")
+            true_items.append(AnnouncementTagsEnum.ALLOW_PETS)
         if announcement.has_furniture:
-            true_items.append("HAS_FORNITURE")
+            true_items.append(AnnouncementTagsEnum.HAS_FURNITURE)
         if announcement.has_internet:
-            true_items.append("HAS_INTERNET")
+            true_items.append(AnnouncementTagsEnum.HAS_INTERNET)
         if announcement.has_piped_gas:
-            true_items.append("HAS_PIPED_GAS")
+            true_items.append(AnnouncementTagsEnum.HAS_PIPED_GAS)
         if announcement.is_close_to_supermarket:
-            true_items.append("IS_CLOSE_TO_SUPERMARKET")
+            true_items.append(AnnouncementTagsEnum.IS_CLOSE_TO_SUPERMARKET)
         if announcement.is_close_to_university:
-            true_items.append("IS_CLOSE_TO_UNIVERSITY")
+            true_items.append(AnnouncementTagsEnum.IS_CLOSE_TO_UNIVERSITY)
         
         for vacancy in announcement.vacancies:
             if vacancy.allowed_smoker:
-                true_items.append("ALLOWED_SMOKER")
+                true_items.append(AnnouncementTagsEnum.ALLOWED_SMOKER)
             if vacancy.has_bathroom:
-                true_items.append("HAS_BATHROOM")
+                true_items.append(AnnouncementTagsEnum.HAS_BATHROOM)
             if vacancy.has_cable_internet:
-                true_items.append("HAS_CABLE_INTERNET")
+                true_items.append(AnnouncementTagsEnum.HAS_CABLE_INTERNET)
             if vacancy.is_shared_room:
-                true_items.append("IS_SHARED_ROOM")
+                true_items.append(AnnouncementTagsEnum.IS_SHARED_ROOM)
             if vacancy.required_extroverted_person:
-                true_items.append("REQUIRED_EXTROVERTED_PERSON")
+                true_items.append(AnnouncementTagsEnum.REQUIRED_EXTROVERTED_PERSON)
+            if vacancy.required_organized_person:
+                true_items.append(AnnouncementTagsEnum.REQUIRED_ORGANIZED_PERSON)
             
             if vacancy.gender == GenderEnum.FEMALE:
-                true_items.append("FEMALE_GENDER")
+                true_items.append(AnnouncementTagsEnum.FEMALE_GENDER)
             elif vacancy.gender == GenderEnum.MALE:
-                true_items.append("MALE_GENDER")
+                true_items.append(AnnouncementTagsEnum.MALE_GENDER)
         
         return true_items
 
