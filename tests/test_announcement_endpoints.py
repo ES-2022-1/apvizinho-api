8e503616791fc37fb275b22596f4dc85579b0789
José Igor de Farias Gomes <jose.gomes@ccc.ufcg.edu.br>
2022-07-17 11:27:49 -0300

    cria teste para listagem de anuncio por filtro

--- tests/test_announcement_endpoints.py
+++ tests/test_announcement_endpoints.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from app.announcement.schemas.announcement import AnnouncementTypeEnum, StatusEnum
+from app.announcement.schemas.announcement import AnnouncementTypeEnum, StatusEnum, AnnouncementFilter
 
 from .base_client import BaseClient
 
@@ -10,6 +10,9 @@ from .base_client import BaseClient
 class AnnouncementClient(BaseClient):
     def __init__(self, client):
         super().__init__(client, endpoint_path="announcement")
+
+    def list_announcements_by_filter(self, announcement_filter: AnnouncementFilter):
+        return self.client.get(f"{self.path}/filter", headers=self.headers, data=announcement_filter)
 
 
 @pytest.fixture
@@ -121,3 +124,129 @@ def test_list_announcements(announcement, session, client):
 
     assert response.status_code == 200
     assert len(response.json()) == 1
+
+def test_list_announcements_by_filter(make_user, session, client):
+    user = make_user()
+    session.add(user)
+    session.commit()
+
+    data_anuncio1 = {
+        "title": "anuncio 1",
+        "description": "string",
+        "is_close_to_university": True,
+        "is_close_to_supermarket": True,
+        "has_furniture": False,
+        "has_internet": False,
+        "allow_pet": True,
+        "allow_events": True,
+        "has_piped_gas": False,
+        "status": "ACTIVE",
+        "type": "HOUSE",
+        "id_user": user.id_user,
+        "address": {
+            "street": "Av. Ricardo Wagner da Silva Paz",
+            "city": "Campina Grande",
+            "number": "200",
+            "complement": "",
+            "zip_code": "58429110",
+        },
+        "vacancies": [
+            {
+                "has_bathroom": False,
+                "has_garage": True,
+                "has_furniture": False,
+                "has_cable_internet": False,
+                "is_shared_room": True,
+                "allowed_smoker": False,
+                "required_organized_person": False,
+                "required_extroverted_person": False,
+                "gender": "FEMALE",
+                "price": 0,
+            }
+        ],
+    }
+
+    data_anuncio2 = {
+        "title": "anuncio 2",
+        "description": "string",
+        "is_close_to_university": False,
+        "is_close_to_supermarket": True,
+        "has_furniture": True,
+        "has_internet": False,
+        "allow_pet": True,
+        "allow_events": True,
+        "has_piped_gas": False,
+        "status": "ACTIVE",
+        "type": "HOUSE",
+        "id_user": user.id_user,
+        "address": {
+            "street": "Rua Bolivia",
+            "city": "Campina Grande",
+            "number": "380",
+            "complement": "",
+            "zip_code": "58416543",
+        },
+        "vacancies": [
+            {
+                "has_bathroom": False,
+                "has_garage": False,
+                "has_furniture": True,
+                "has_cable_internet": False,
+                "is_shared_room": False,
+                "allowed_smoker": True,
+                "required_organized_person": False,
+                "required_extroverted_person": False,
+                "gender": "FEMALE",
+                "price": 0,
+            }
+        ],
+    }
+
+    data_anuncio3 = {
+        "title": "anuncio 3",
+        "description": "string",
+        "is_close_to_university": True,
+        "is_close_to_supermarket": True,
+        "has_furniture": False,
+        "has_internet": True,
+        "allow_pet": True,
+        "allow_events": True,
+        "has_piped_gas": True,
+        "status": "ACTIVE",
+        "type": "HOUSE",
+        "id_user": user.id_user,
+        "address": {
+            "street": "R. Antônio Joaquim Pequeno",
+            "city": "Campina Grande",
+            "number": "595",
+            "complement": "",
+            "zip_code": "58429105",
+        },
+        "vacancies": [
+            {
+                "has_bathroom": True,
+                "has_garage": True,
+                "has_furniture": True,
+                "has_cable_internet": True,
+                "is_shared_room": False,
+                "allowed_smoker": False,
+                "required_organized_person": True,
+                "required_extroverted_person": False,
+                "gender": "FEMALE",
+                "price": 0,
+            }
+        ],
+    }
+
+    client.create(json.dumps(data_anuncio1))
+    client.create(json.dumps(data_anuncio2))
+    client.create(json.dumps(data_anuncio3))
+
+    filters = AnnouncementFilter(filters=["IS_CLOSE_TO_UNIVERSITY", "IS_CLOSE_TO_SUPERMARKET", "ALLOW_PET", "FEMALE_GENDER"])
+
+    response = client.list_announcements_by_filter(filters)
+
+    assert response.status_code == 200
+    assert list(map(lambda x: x.title, response.json())) == ["anuncio 3", "anuncio 1", "anuncio 2"]
+    
+
