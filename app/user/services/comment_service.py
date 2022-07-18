31d1b762dd969251de5b92c219babff270f45cb6
ricardoadley <ricardo.sena@ccc.ufcg.edu.br>
2022-07-17 23:06:51 -0300

    fix: fix wrong repository on comment service

--- app/user/services/comment_service.py
+++ app/user/services/comment_service.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 
 from app.common.services.base import BaseService
-from app.user.repositories.review_repository import ReviewRepository
+from app.user.repositories.comment_repository import CommentRepository
 from app.user.schemas import CommentCreate, CommentView
 from app.user.schemas.comment import CommentUpdate
 
@@ -9,6 +9,6 @@ from app.user.schemas.comment import CommentUpdate
 class CommentService(BaseService[CommentCreate, CommentUpdate, CommentView]):
     def __init__(self, db: Session):
         super().__init__(
-            repository=ReviewRepository,
+            repository=CommentRepository,
             db=db,
         )
